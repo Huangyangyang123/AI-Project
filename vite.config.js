418642413825d4cdb572adff747f57ad92b8d53a
Summer Huang <13524382551@163.com>
2025-03-11 11:59:50 +0800

    feat:project init

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
@@ -10,20 +11,39 @@ export default defineConfig({
         javascriptEnabled:true,
         math: 'parens-division',
         modifyVars:{
-          // hack:`true;@import '@styles/vars.less`,
-          // 'root-entry-name':'default'
+          hack:`true; @import '@/styles/vars.less'`,
+          'root-entry-name':'default'
         }
       }
     }
   },
   server:{
-    proxy:'',
+    open:true,
+    proxy:{
+      '/api':{
+        target:'',
+        changeOrigin:true,
+        secure:false,
+        rewrite:(path)=>path.replace(/^\/api/,'')
+      },
+    },
 
   },
   resolve:{
-    alias:''
+    alias:{
+      '@':resolve(__dirname,'./src')
+    }
   },
   build:{
-
+    rollupOptions:{
+      output:{
+        manualChunks:{
+          react:['react','react-dom','react-router-dom'],
+          antd:['antd','@ant-design/icons'],
+          mobx:['mobx','mobx-react-lite']
+        }
+      }
+    },
+    target:['edge90','chrome90','firefox90','safari15']
   }
 })
